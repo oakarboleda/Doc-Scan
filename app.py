43641f089278fd2bcf191292b6efec4b550cb7bb
Oak Arboleda <odedenarboleda@san.com>
2025-04-22 18:45:09 -0500

    Update login screen and .gitignore for Streamlit app
    Signed-off-by: Oak Arboleda <odedenarboleda@san.com>

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ def login_screen():
     st.subheader("Please log in.")
     st.button("Log in with Google", on_click=st.login)
 
-if not st.experimental_user.is_logged_in:
+if not st.experimental_user.is_authenticated:
     login_screen()
 else:
     st.header(f"Welcome, {st.experimental_user.name}!")
