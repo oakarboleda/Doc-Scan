a811b7717a4e4faf72c17bc3b823c54eade6542b
Oak Arboleda <oakarboleda@gmail.com>
2025-05-05 09:27:07 -0500

    Add .env to .gitignore and simplify app.py setup
    Updated .gitignore to exclude .env files for better security. Simplified app.py by removing unused configuration and clarifying navigation initialization.
    
    Signed-off-by: Oak Arboleda <oakarboleda@gmail.com>

--- app.py
+++ app.py
@@ -1,16 +1,7 @@
 import streamlit as st
 from st_pages import add_page_title, get_nav_from_toml
 
-st.set_page_config(
-    page_title="Document Scanner App",
-    page_icon="🧊",
-    layout="wide",
-    initial_sidebar_state="expanded",
 
-)
-# If you want to use the no-sections version, this
-# defaults to looking in .streamlit/pages.toml, so you can
-# just call `get_nav_from_toml()`
 nav = get_nav_from_toml()
 
 
