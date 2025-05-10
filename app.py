46407b7d1841f51a3604a08a422e608dd99d2851
Oak Arboleda <oakarboleda@gmail.com>
2025-05-10 15:49:16 -0500

    Update session state handling to use `st.user` for login
    Replaced `st.session_state` with `st.user` for login status management. This aligns with updated API conventions and improves code clarity and maintainability.
    
    Signed-off-by: Oak Arboleda <oakarboleda@gmail.com>

--- app.py
+++ app.py
@@ -12,12 +12,12 @@ else:
 
 def login():
     if st.button("Log in"):
-        st.session_state.logged_in = True
+        st.user.logged_in = True
         st.rerun()
 
 def logout():
     if st.button("Log out"):
-        st.session_state.logged_in = False
+        st.user.logged_in = False
         st.rerun()
 
 
@@ -25,7 +25,7 @@ dashboard = st.Page("pages/dashboard.py", title="Dashboard", icon=":material/das
 upload = st.Page("pages/upload.py", title="Upload", icon=":material/upload:")
 # analysis = st.Page("pages/analysis.py", title="analysis", icon=":material/bar_chart:")
 # Main app logic
-if st.session_state.logged_in:
+if st.user.logged_in:
     pg = st.navigation(
         {
             "Dashboard": [dashboard],
